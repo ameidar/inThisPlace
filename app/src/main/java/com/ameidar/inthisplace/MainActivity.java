e3a3fddd1f7335c9cff36879884c6a11b6daaedc
ameidar <ami.meidar@gmail.com>
2018-08-16 18:25:38 +0300

    Json dependency from google

--- app/src/main/java/com/ameidar/inthisplace/MainActivity.java
+++ app/src/main/java/com/ameidar/inthisplace/MainActivity.java
@@ -1,8 +1,14 @@
 package com.ameidar.inthisplace;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int SIGNIN_REQUEST = 1001;
     public static final String MY_GLOBAL_PREFS = "my_global_prefs";
+    public static final  int REQUEST_PERMISSION_WRITE = 1002;
+    private boolean permissionGranted;
     TextView tvOut;
     List<String> itemNames = new ArrayList<>(  );
     List<DataItem> dataItemList = sampleDataProvider.dataItemList ;
@@ -37,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        //tvOut = (TextView) findViewById(R.id.out);
-        //tvOut.setText("");
+
+        checkPermissions();
+
         Collections.sort( dataItemList, new Comparator<DataItem>() {
             @Override
             public int compare(DataItem o1, DataItem o2) {
@@ -91,6 +100,12 @@ public class MainActivity extends AppCompatActivity {
                 Intent settingIntent = new Intent( this , PrefsActivity.class );
                 startActivity( settingIntent );
                 return true ;
+            case R.id.action_import:
+                return true ;
+            case R.id.action_export:
+//                boolean result = null ;
+                return  true;
+
 
         }
         return super.onOptionsItemSelected( item );
@@ -112,5 +127,60 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             Toast.makeText( this, "request code error", Toast.LENGTH_SHORT ).show();
+    }
+
+
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+
+    // Initiate request for permissions.
+    private boolean checkPermissions() {
+
+        if (!isExternalStorageReadable() || !isExternalStorageWritable()) {
+            Toast.makeText(this, "This app only works on devices with usable external storage",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_WRITE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_WRITE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                    Toast.makeText(this, "External storage permission granted",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "You must grant permission!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
